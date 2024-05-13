0c576066a7e55e1b1b6976fcb487eaee0a0c463c
Mohitag403 <149030402+Mohitag403@users.noreply.github.com>
2024-05-13 22:50:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,20 +4,20 @@
 import os
 from os import getenv
 # ---------------R---------------------------------
-API_ID = int(os.environ.get("API_ID"))
+API_ID = int(os.environ.get("API_ID", "16874790"))
 # ------------------------------------------------
-API_HASH = os.environ.get("API_HASH")
+API_HASH = os.environ.get("API_HASH", "46aa49adca0f1d184eb2a2f4a48a1df9")
 # ----------------D--------------------------------
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6770739862:AAEJnHU9R-B9vB71xWoh6VfvqMFAzwJc-Lw")
 # -----------------A-------------------------------
-BOT_USERNAME = os.environ.get("BOT_USERNAME")
+BOT_USERNAME = os.environ.get("BOT_USERNAME", "EQUROBOT")
 # ------------------X------------------------------
-OWNER_ID = int(os.environ.get("OWNER_ID"))
+OWNER_ID = int(os.environ.get("OWNER_ID", "6664582540"))
 # ------------------X------------------------------
-DEEP_API = os.environ.get("DEEP_API")
+DEEP_API = os.environ.get("DEEP_API", "5163c49d-b696-47f1-8cf9-4801738436dd")
 # ------------------------------------------------
-LOGGER_ID = int(os.environ.get("LOGGER_ID"))
+LOGGER_ID = int(os.environ.get("LOGGER_ID", "-1001919135283"))
 # ------------------------------------------------
-GPT_API = os.environ.get("GPT_API")
+GPT_API = os.environ.get("GPT_API", "696-47f1-8cf9-4801738436dd")
 # ------------------------------------------------
-DAXX_API = os.environ.get("DAXX_API")
+DAXX_API = os.environ.get("DAXX_API", "5163c49d-b696-47f1-8cf9-")
