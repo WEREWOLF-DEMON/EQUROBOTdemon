2320aebc3f26f5bcd0df780698c5882c31c509eb
ᴹᴿᴅᴀxx ࿐™ <147141400+DAXXTEAM@users.noreply.github.com>
2024-06-08 01:50:49 -0700

    s

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ BOT_TOKEN = os.environ.get("BOT_TOKEN", "6770739862:AAEJnHU9R-B9vB71xWoh6VfvqMFA
 # -----------------A-------------------------------
 BOT_USERNAME = os.environ.get("BOT_USERNAME", "EQUROBOT")
 # ------------------X------------------------------
-OWNER_ID = int(os.environ.get("OWNER_ID", "6664582540"))
+OWNER_ID = int(os.environ.get("OWNER_ID", "7427691214"))
 # ------------------X------------------------------
 DEEP_API = os.environ.get("DEEP_API", "5163c49d-b696-47f1-8cf9-4801738436dd")
 # ------------------------------------------------
