514d84f03869ea9593affbbd952402572dce0cc0
Mohitag403 <149030402+Mohitag403@users.noreply.github.com>
2024-06-14 22:53:38 +0530

    Update bin.py

--- EQUROBOT/modules/bin.py
+++ EQUROBOT/modules/bin.py
@@ -17,11 +17,11 @@ async def bin_lookup(client, message):
     bin_info = f"""
 𝗕𝗜𝗡 𝗟𝗼𝗼𝗸𝘂𝗽 𝗥𝗲𝘀𝘂𝗹𝘁 🔍
 
-𝗕𝗜𝗡 ⇾ {bin_number}
+𝗕𝗜𝗡 ⇾ `{bin_number}`
 
-𝗜𝗻𝗳𝗼 ⇾ {data.get('scheme', 'N/A').upper()} - {data.get('type', 'N/A').upper()} - {data.get('brand', 'N/A').upper()}
-𝐈𝐬𝐬𝐮𝐞𝐫 ⇾ {data.get('bank', {}).get('name', 'N/A').upper()}
-𝐂𝐨𝐮𝐧𝐭𝐫𝐲 ⇾ {data.get('country', {}).get('name', 'N/A').upper()} {data.get('country', {}).get('emoji', '')}
+𝗜𝗻𝗳𝗼 ⇾ `{data.get('scheme', 'N/A').upper()} - {data.get('type', 'N/A').upper()} - {data.get('brand', 'N/A').upper()}`
+𝐈𝐬𝐬𝐮𝐞𝐫 ⇾ `{data.get('bank', {}).get('name', 'N/A').upper()}`
+𝐂𝐨𝐮𝐧𝐭𝐫𝐲 ⇾ `{data.get('country', {}).get('name', 'N/A').upper()} {data.get('country', {}).get('emoji', '')}`
 """
 
     await message.reply_text(bin_info)
