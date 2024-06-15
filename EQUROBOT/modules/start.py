f70b2e81f9c4ce9547cb9cdfe8ff1aec7b5c3931
AbhiModszYT <133230865+AbhiModszYT@users.noreply.github.com>
2024-06-16 00:22:13 +0530

    Update start.py

--- EQUROBOT/modules/start.py
+++ EQUROBOT/modules/start.py
@@ -1,40 +1,154 @@
-from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, InputMediaPhoto
-from config import BOT_USERNAME, OWNER_ID
-from pyrogram.types import InlineKeyboardButton as ib
+from pyrogram import Client, filters, enums
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message, CallbackQuery
+from pyrogram import Client, errors
+from pyrogram.enums import ChatMemberStatus, ParseMode
+from pyrogram.enums import ChatType
 import asyncio
+import os
+from os import getenv
+import traceback
+from pyrogram import filters, Client
+from pyrogram.types import Message
+from unidecode import unidecode
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+import random 
+import time
+import random
 from EQUROBOT import app
+from config import BOT_USERNAME, OWNER_ID
 
 
-START_TEXT = """
-ʜɪ ,
-
+AM_PIC = [
+    "https://graph.org/file/ed513284c18489abfaf5f.jpg",
+    "https://graph.org/file/ed513284c18489abfaf5f.jpg",
+    
+]
+ban_txt = """
+ʜɪ {} ,
 ɪ ᴀᴍ , 
 ʏᴏᴜʀ ᴀɪ ᴄᴏᴍᴘᴀɴɪᴏɴ. 
 ʟᴇᴛ'ꜱ ᴄʜᴀᴛ ᴀɴᴅ ᴇxᴘʟᴏʀᴇ 
 ᴛʜᴇ ᴅᴇᴘᴛʜꜱ ᴏꜰ ᴄᴏɴᴠᴇʀꜱᴀᴛɪᴏɴ ᴛᴏɢᴇᴛʜᴇʀ! 
 ꜰᴇᴇʟ ꜰʀᴇᴇ ᴛᴏ ᴀꜱᴋ ᴍᴇ ᴀɴʏᴛʜɪɴɢ ᴏʀ ꜱʜᴀʀᴇ ʏᴏᴜʀ ᴛʜᴏᴜɢʜᴛꜱ. 
-ɪ'ᴍ ʜᴇʀᴇ ᴛᴏ ʟɪꜱᴛᴇɴ ᴀɴᴅ ᴇɴɢᴀɢᴇ ɪɴ ᴍᴇᴀɴɪɴɢꜰᴜʟ ᴅɪꜱᴄᴜꜱꜱɪᴏɴꜱ ᴡɪᴛʜ ʏᴏᴜ."
+ɪ'ᴍ ʜᴇʀᴇ ᴛᴏ ʟɪꜱᴛᴇɴ ᴀɴᴅ ᴇɴɢᴀɢᴇ ɪɴ ᴍᴇᴀɴɪɴɢꜰᴜʟ ᴅɪꜱᴄᴜꜱꜱɪᴏɴꜱ ᴡɪᴛʜ ʏᴏᴜ.
 """
+help_txt = """
+» ʙᴏᴛ ғᴇᴀᴛᴜʀᴇs.
+"""
+killall_txt = """
+1. <code>/start</code> - start bot
 
+ɴᴏᴡ ʙᴏᴛ ᴡɪʟʟ ᴡᴏʀᴋɪɴɢ ɪɴ ɢʀᴏᴜᴘ ✅.
+"""
+Rbanall = """
+1. <code>/start</code> - start bot
 
+ɴᴏᴡ ʙᴏᴛ ᴡɪʟʟ ᴡᴏʀᴋɪɴɢ ɪɴ ɢʀᴏᴜᴘ ✅.
 
-@app.on_message(filters.command("start") & filters.private)
-async def start(client, message):
-    buttons = [
+ɴᴏᴛᴇ : ᴛʜɪꜱ ᴄᴍᴅꜱ ᴏɴʟʏ ᴡᴏʀᴋ ɪɴ ʙᴏᴛ ᴘᴍ.
+"""
+app_buttons = [
+
+                [ 
+                    InlineKeyboardButton("ʙᴀɴᴀʟʟ", callback_data="banall_"),
+                    InlineKeyboardButton("ʀʙᴀɴᴀʟʟ", callback_data="rbanall_"),
+        
+                ],
+                [
+                    InlineKeyboardButton("⟲ ʙᴀᴄᴋ ⟳", callback_data="home_"),
+                    InlineKeyboardButton("⟲ ᴄʟᴏꜱᴇ ⟳", callback_data="close_data")
+                ]
+                ]
+
+back_buttons  = [[
+                    InlineKeyboardButton("⟲ ʙᴀᴄᴋ ⟳", callback_data="help_"),                    
+                ]]
+
+button = InlineKeyboardMarkup([
         [
-            InlineKeyboardButton("⦿ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ⦿", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")
+            InlineKeyboardButton("⦿ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ⦿", url=f"https://t.me/{app.me.username}?startgroup=true")
         ],
         [
-            InlineKeyboardButton("⦿ɢʀᴏᴜᴘ⦿", url=f"https://t.me/CARD3DBOTx"),
-            InlineKeyboardButton("⦿ᴏᴡɴᴇʀ⦿", user_id=OWNER_ID)
+            InlineKeyboardButton("⦿ɢʀᴏᴜᴘ⦿", url=f"https://t.me/ALLTYPECC"),    
+        ],
+       [
+            InlineKeyboardButton("⦿ ᴏᴡɴᴇʀ ⦿", user_id=OWNER_ID)  
+        ],
+    [
+           InlineKeyboardButton("ᴄᴏᴍᴍᴀɴᴅs", callback_data="help_"),    
+      ]
+    
+])
+
+@Client.on_message(filters.command(["start"], prefixes=[".","/","!"]) & filters.private)
+async def start(_, message):
+    await message.reply_photo(
+        photo=random.choice(AM_PIC),
+        caption=ban_txt.format(message.from_user.mention, message.from_user.id),
+        reply_markup=button
+    )    
+
+@Client.on_callback_query()
+async def cb_handler(client, query):
+    if query.data=="home_":
+        buttons =  [
+            [
+            InlineKeyboardButton("⦿ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ⦿", url=f"https://t.me/{app.me.username}?startgroup=true")
+        ],
+            [
+            InlineKeyboardButton("⦿ɢʀᴏᴜᴘ⦿", url=f"https://t.me/ALLTYPECC"),    
+        ],
+            [
+            InlineKeyboardButton("⦿ ᴏᴡɴᴇʀ ⦿", user_id=OWNER_ID)  
+        ],
+            [
+                InlineKeyboardButton("ᴄᴏᴍᴍᴀɴᴅs", callback_data="help_")
+            ]    
         ]
-    ]
+        reply_markup = InlineKeyboardMarkup(buttons)
+        try:
+            await query.edit_message_text(
+                ban_txt.format(query.from_user.mention, query.from_user.id),
+                reply_markup=reply_markup
+            )
+        except MessageNotModified:
+            pass
 
-    reply_markup = InlineKeyboardMarkup(buttons)
+    elif query.data=="help_":        
+        reply_markup = InlineKeyboardMarkup(app_buttons)
+        try:
+            await query.edit_message_text(
+                help_txt,
+                reply_markup=reply_markup
+            )
+        except MessageNotModified:
+            pass
 
-    await message.reply_video(
-        video="https://telegra.ph/file/365de71e032aadb98e1d2.mp4",
-        caption=START_TEXT,
-        reply_markup=reply_markup
-    )
+
+
+    elif query.data=="banall_":        
+        reply_markup = InlineKeyboardMarkup(back_buttons)
+        try:
+            await query.edit_message_text(
+                killall_txt,
+                reply_markup=reply_markup
+            )
+        except MessageNotModified:
+            pass
+
+    elif query.data=="rbanall_":        
+        reply_markup = InlineKeyboardMarkup(back_buttons)
+        try:
+            await query.edit_message_text(
+                Rbanall,
+                reply_markup=reply_markup
+            )
+        except MessageNotModified:
+            pass
+            
+    elif query.data=="close_data":
+        try:
+            await query.message.delete()
+            await query.message.reply_to_message.delete()
+        except:
+            pass
