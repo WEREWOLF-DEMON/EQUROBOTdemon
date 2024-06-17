f6e832959ed88e0d16ca453e758efec448485833
ᴹᴿᴅᴀxx ࿐™ <147141400+DAXXTEAM@users.noreply.github.com>
2024-06-18 01:04:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ API_ID = int(os.environ.get("API_ID", "16874790"))
 # ------------------------------------------------
 API_HASH = os.environ.get("API_HASH", "46aa49adca0f1d184eb2a2f4a48a1df9")
 # ----------------D--------------------------------
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7109817776:AAGylSq_BS5gMf6dUyzyZZ-adXKi5SD9OX0")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7384821138:AAGSkPUkQ0EPiHnUDOCHePRecpVPVYxRFBM")
 # -----------------A-------------------------------
 BOT_USERNAME = os.environ.get("BOT_USERNAME", "EQUROBOT")
 # ------------------X------------------------------
