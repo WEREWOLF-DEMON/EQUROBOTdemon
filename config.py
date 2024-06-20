13ca5fbaae487c6db2ab67235f0a77119a4e6341
ᴹᴿᴅᴀxx ࿐™ <147141400+DAXXTEAM@users.noreply.github.com>
2024-06-20 22:40:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,9 +8,9 @@ API_ID = int(os.environ.get("API_ID", "16874790"))
 # ------------------------------------------------
 API_HASH = os.environ.get("API_HASH", "46aa49adca0f1d184eb2a2f4a48a1df9")
 # ----------------D--------------------------------
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7386696229:AAHTbBQ9KmZZaBSRMwjdL2s80uFWqvm3FyY")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7109817776:AAFG1MI-eRurvUOgOQOZkvcMo_9ksYQgfWE")
 # -----------------A-------------------------------
-BOT_USERNAME = os.environ.get("BOT_USERNAME", "CARD3DBOT")
+BOT_USERNAME = os.environ.get("BOT_USERNAME", "EQUROBOT")
 # ------------------X------------------------------
 OWNER_ID = int(os.environ.get("OWNER_ID", "7427691214"))
 
