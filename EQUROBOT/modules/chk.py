1f98c022a67d2f1e455253cebb7b538cf562e52f
ᴹᴿᴅᴀxx ࿐™ <147141400+DAXXTEAM@users.noreply.github.com>
2024-07-01 03:39:15 +0530

    Update chk.py

--- EQUROBOT/modules/chk.py
+++ EQUROBOT/modules/chk.py
@@ -35,7 +35,7 @@ async def check_cc(_, message):
     url = "https://mvy.ai/sk_api/api.php"
     params = {
         "lista": f"{ccn}:{mm}:{yy}:{cvv}",
-        "sk": "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"
+        "sk": "sk_live_v6hZVe0J4f3rShGDqOSiwh8v"
     }
 
     try:
