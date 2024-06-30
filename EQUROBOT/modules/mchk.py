798b26b42babfed78f111e25cf66424bcc72a554
ᴹᴿᴅᴀxx ࿐™ <147141400+DAXXTEAM@users.noreply.github.com>
2024-07-01 03:38:40 +0530

    Update mchk.py

--- EQUROBOT/modules/mchk.py
+++ EQUROBOT/modules/mchk.py
@@ -25,7 +25,7 @@ async def process_credit_card(cc_entry):
             url = "https://mvy.ai/sk_api/api.php"
             params = {
                 "lista": f"{ccn}:{mm}:{yy}:{cvv}",
-                "sk": "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"
+                "sk": "sk_live_v6hZVe0J4f3rShGDqOSiwh8v"
             }
 
             async with session.get(url, params=params) as response:
