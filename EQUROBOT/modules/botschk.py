357b55b15a94b9fe3666663f14c1376421245f33
Mohitag403 <149030402+Mohitag403@users.noreply.github.com>
2024-07-01 22:54:25 +0530

    Update botschk.py

--- EQUROBOT/modules/botschk.py
+++ EQUROBOT/modules/botschk.py
@@ -1,7 +1,7 @@
 import asyncio
 import random
 from pyrogram import filters
-from EQUROBOT import app, scr2
+from EQUROBOT import app, scr
 from config import OWNER_ID, EVAL as OWNER_ID
 
 BOT_LIST = ["EQUROBOT", "YesikooBot"]
@@ -13,12 +13,12 @@ async def bots_chk(_, message):
     response = "**ᴄʜᴇᴄᴋɪɴɢ ʙᴏᴛs sᴛᴀᴛs ᴀʟɪᴠᴇ ᴏʀ ᴅᴇᴀᴅ**\n\n"
     for bot_username in BOT_LIST:
         try:
-            bot = await scr2.get_users(bot_username)
+            bot = await scr.get_users(bot_username)
             bot_id = bot.id
             await asyncio.sleep(0.5)
-            bot_info = await scr2.send_message(bot_id, "/start")
+            bot_info = await scr.send_message(bot_id, "/start")
             await asyncio.sleep(3)
-            async for bot_message in scr2.get_chat_history(bot_id, limit=1):
+            async for bot_message in scr.get_chat_history(bot_id, limit=1):
                 if bot_message.from_user.id == bot_id:
                     response += f"╭⎋ [{bot.first_name}](tg://user?id={bot.id})\n╰⊚ **sᴛᴀᴛᴜs: ᴏɴʟɪɴᴇ ✨**\n\n"
                 else:
