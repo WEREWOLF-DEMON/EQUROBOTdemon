42fe9a6de6c134407cbc8310866d8b028d70dbe4
ᴹᴿᴅᴀxx ࿐™ <147141400+DAXXTEAM@users.noreply.github.com>
2024-07-01 18:40:20 +0530

    Update botschk.py

--- EQUROBOT/modules/botschk.py
+++ EQUROBOT/modules/botschk.py
@@ -8,7 +8,7 @@ BOT_LIST = ["EQUROBOT", "YesikooBot"]
 
 
 
-@Nexus.on_message(filters.command("botschk") & filters.user(OWNER_ID))
+@app.on_message(filters.command("botschk") & filters.user(OWNER_ID))
 async def bots_chk(_, message):
     msg = await message.reply_photo(photo="https://telegra.ph/file/4d303296e4fac9a40ea07.jpg", caption="**ᴄʜᴇᴄᴋɪɴɢ ʙᴏᴛs sᴛᴀᴛs ᴀʟɪᴠᴇ ᴏʀ ᴅᴇᴀᴅ...**")
     response = "**ᴄʜᴇᴄᴋɪɴɢ ʙᴏᴛs sᴛᴀᴛs ᴀʟɪᴠᴇ ᴏʀ ᴅᴇᴀᴅ**\n\n"
