741d0d82c1c40113aab8af2760b49b36d127a3b3
ᴹᴿᴅᴀxx ࿐™ <147141400+DAXXTEAM@users.noreply.github.com>
2024-07-01 19:12:44 +0530

    Update botschk.py

--- EQUROBOT/modules/botschk.py
+++ EQUROBOT/modules/botschk.py
@@ -1,11 +1,11 @@
 import asyncio
 from pyrogram import filters
-from EQUROBOT import app, scr2
+from EQUROBOT import app as bot, scr2
 from config import OWNER_ID
 
 BOT_LIST = ["EQUROBOT", "YesikooBot"]
 
-@app.on_message(filters.command("botschk") & filters.user(OWNER_ID))
+@bot.on_message(filters.command("botschk") & filters.user(OWNER_ID))
 async def bots_chk(_, message):
     msg = await message.reply_photo(photo="https://telegra.ph/file/4d303296e4fac9a40ea07.jpg", caption="**ᴄʜᴇᴄᴋɪɴɢ ʙᴏᴛs sᴛᴀᴛs ᴀʟɪᴠᴇ ᴏʀ ᴅᴇᴀᴅ...**")
     response = "**ᴄʜᴇᴄᴋɪɴɢ ʙᴏᴛs sᴛᴀᴛs ᴀʟɪᴠᴇ ᴏʀ ᴅᴇᴀᴅ**\n\n"
