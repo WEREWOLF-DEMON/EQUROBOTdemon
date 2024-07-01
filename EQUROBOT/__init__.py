0c1ea76133d4474321ef85a28813462445ee891e
ᴹᴿᴅᴀxx ࿐™ <147141400+DAXXTEAM@users.noreply.github.com>
2024-07-01 18:14:36 +0530

    Update __init__.py

--- EQUROBOT/__init__.py
+++ EQUROBOT/__init__.py
@@ -36,6 +36,13 @@ scr = Client(
     session_string="BQC6kfsAHYAX81Yg61rP01K9FPavjslDQ-5CLkaiEuqNXhRwS5BOJQFhrxeaZr0oZfI5FJBJKz2Mj8SNtmFe08nsUnJN8JrvoAlc5zxEhshWNJP5M4_7qjgVy8Q2Z12AA0od8iSAyDQkJZnNsCk73Mv53alNvPqeo8TIxC_v4SEMicpU8RUipxLKt57S_q15B2_amDN8cu2pXlf0Hhj770wwYV-JPLQ8eIpRsaJ_8NTBNfXkvlS_rdVKSboA99XnSkLo--25erheCBAGXf1wKtOWgXUjxE3HmJz8-Ppjo_mB459H9A215LOOEjyl7nLTaQGuBS6yxtpPCT-qKg_wDAQTnXnHrgAAAAGkC4T6AA"
 )
 
+scr2 = Client(
+    "scr2",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    session_string="AQAaqk4Afm17ws0m_fBUAsI2o5LKf1E0I2RZIDryt-ZnKMMw0Fz7d_GH3pK-cllAigJDnnhW8b66SxmCf2m4Gxl9QiuCkrX3dwEDAFwgUrU5fUPUzCf-rCYUdw4bRGrL8BNcEZn8ezHznlMDaylo6hP71De0d557tlPbIr7lQInPhqo7-r7I_s0JtMgaajXd4H9gz1xqX1dorXG8EY9qVIfCBz6SQjpfHxKVpcoA0Dh_ClHukGMySKMCQqPczVmz3TJnlCp25RNn5hwYGRFs3gGTsofYS7DpjmCEn06MsbmjXB4co3nJRzD35IBZ-7VChlzxjFQWEO0FLWX8-VAEHOJkRX4ArQAAAAGE_Y--AA"
+)
+
 
 
 async def info_bot():
