877c068a3c70bf6f55de07d44abe7f3ebc02becb
ᴹᴿᴅᴀxx ࿐™ <147141400+DAXXTEAM@users.noreply.github.com>
2024-07-08 16:53:23 +0530

    Update HQdump.py

--- EQUROBOT/modules/HQdump.py
+++ EQUROBOT/modules/HQdump.py
@@ -79,7 +79,7 @@ async def dump_cards(client, message):
     try:
         amount = int(message.command[1])
     except (IndexError, ValueError):
-        await message.reply_text("Pʟᴇᴀsᴇ Pʀᴏᴠɪᴅᴇ ᴍᴇ ᴀ ᴠᴀɪʟᴅ ᴄᴏᴍᴍᴀɴᴅ ᴛᴏ Dᴜᴍᴘ Cᴄ ᴜsᴇ /dump 3000")        
+        await message.reply_text("Pʟᴇᴀsᴇ Pʀᴏᴠɪᴅᴇ ᴍᴇ ᴀ ᴠᴀɪʟᴅ ᴄᴏᴍᴍᴀɴᴅ ᴛᴏ Dᴜᴍᴘ Cᴄ ᴜsᴇ /hqdump 3000")        
         return
 
     file_path = f"{amount}x_HQ_CC_Dumped_By_@{BOT_USERNAME}.txt"
