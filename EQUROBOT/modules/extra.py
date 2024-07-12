eccd4fc72898d974fb30ccbf5d0635b887a88907
ᴹᴿᴅᴀxx ࿐™ <147141400+DAXXTEAM@users.noreply.github.com>
2024-07-12 22:40:59 +0530

    Update and rename id.py to extra.py

--- EQUROBOT/modules/extra.py
+++ EQUROBOT/modules/extra.py
@@ -10,6 +10,7 @@ import httpx
 from pymongo import MongoClient
 import re
 from datetime import datetime
+from gpytranslate import Translator
 ####
 
 @app.on_message(filters.command('id'))
@@ -116,3 +117,41 @@ async def bot_leave(_, message):
     chat_id = message.chat.id
     await message.reply_text("Your bot has successfully left the chat 🙋‍♂️")
     await app.leave_chat(chat_id=chat_id, delete=True)
+
+
+
+# ------------
+
+#.......
+
+trans = Translator()
+
+#......
+
+@app.on_message(filters.command("tr"))
+async def translate(_, message) -> None:
+    reply_msg = message.reply_to_message
+    if not reply_msg:
+        await message.reply_text("ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴍᴇssᴀɢᴇ ᴛᴏ ᴛʀᴀɴsʟᴀᴛᴇ ɪᴛ !")
+        return
+    if reply_msg.caption:
+        to_translate = reply_msg.caption
+    elif reply_msg.text:
+        to_translate = reply_msg.text
+    try:
+        args = message.text.split()[1].lower()
+        if "//" in args:
+            source = args.split("//")[0]
+            dest = args.split("//")[1]
+        else:
+            source = await trans.detect(to_translate)
+            dest = args
+    except IndexError:
+        source = await trans.detect(to_translate)
+        dest = "en"
+    translation = await trans(to_translate, sourcelang=source, targetlang=dest)
+    reply = (
+        f"ᴛʀᴀɴsʟᴀᴛᴇᴅ ғʀᴏᴍ {source} to {dest}:\n"
+        f"{translation.text}"
+    )
+    await message.reply_text(reply)
