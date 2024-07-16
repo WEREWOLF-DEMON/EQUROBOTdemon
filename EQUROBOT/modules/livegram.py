0ec7ef56f5fd49d0d18b1576242a3002dbdf94ac
ᴹᴿᴅᴀxx ࿐™ <147141400+DAXXTEAM@users.noreply.github.com>
2024-07-17 00:42:05 +0530

    Update livegram.py

--- EQUROBOT/modules/livegram.py
+++ EQUROBOT/modules/livegram.py
@@ -4,14 +4,12 @@ from EQUROBOT import app
 from config import OWNER_ID
 
 # Bot owner's Telegram ID
-owner_id = OWNER_ID # Replace with your Telegram ID
-
-
+owner_id = OWNER_ID  # Replace with your Telegram ID
 
 # Function to forward messages to owner
 @app.on_message(filters.private & ~filters.forwarded & ~filters.bot)
 async def forward_to_owner(client, message: Message):
-    await client.forward_messages(chat_id=owner_id, from_chat_id=message.chat.id, message_ids=message.message_id)
+    await client.forward_messages(chat_id=owner_id, from_chat_id=message.chat.id, message_ids=[message.message_id])
 
 # Function to send owner's reply back to original sender
 @app.on_message(filters.chat(owner_id) & ~filters.forwarded)
@@ -21,4 +19,4 @@ async def forward_to_sender(client, message: Message):
         # Get the original sender's chat ID
         original_sender = message.reply_to_message.forward_from.id
         # Forward the reply to the original sender
-        await client.forward_messages(chat_id=original_sender, from_chat_id=message.chat.id, message_ids=message.message_id)
+        await app.forward_messages(chat_id=original_sender, from_chat_id=message.chat.id, message_ids=[message.message_id])
