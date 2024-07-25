6072e7482d762cbeadb032ea55518b1d4fc2c1ec
AbhiModszYT <133230865+AbhiModszYT@users.noreply.github.com>
2024-07-26 01:37:25 +0530

    Update ip.py

--- CloneBoT/Clones/ip.py
+++ CloneBoT/Clones/ip.py
@@ -1,6 +1,6 @@
 from pyrogram import Client, filters
 import requests
-from EQUROBOT import app
+from pyrogram import Client as app, filters, enums
 
 IPINFO_TOKEN = '434e1cea389a93'
 IPQUALITYSCORE_API_KEY = 'Y0OZMypz71dEF9HxxQd21J2xvqUE0BVS'
