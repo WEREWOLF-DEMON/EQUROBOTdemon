dba83260cc81be47beba794f5cfc2fc2bbde8b57
AbhiModszYT <133230865+AbhiModszYT@users.noreply.github.com>
2024-07-26 01:31:39 +0530

    Update dump.py

--- CloneBoT/Clones/dump.py
+++ CloneBoT/Clones/dump.py
@@ -2,7 +2,9 @@ import random
 from datetime import datetime, timedelta
 import os
 from pyrogram import Client, filters
-from EQUROBOT import app, BOT_USERNAME
+from pyrogram import Client as app, filters, enums
+
+BOT_USERNAME =  app.me.username 
 
 def luhn_checksum(card_number):
     def digits_of(n):
