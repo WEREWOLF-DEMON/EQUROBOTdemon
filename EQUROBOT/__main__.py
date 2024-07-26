5c4f947bd61c9827c63975b9d8968c4a413fc28b
AbhiModszYT <133230865+AbhiModszYT@users.noreply.github.com>
2024-07-26 15:23:29 +0530

    Update __main__.py

--- EQUROBOT/__main__.py
+++ EQUROBOT/__main__.py
@@ -1,31 +1,26 @@
 import asyncio
 import importlib
-from pyrogram import Client, idle
+from pyrogram import idle
+from EQUROBOT import app
+from EQUROBOT.modules import ALL_MODULES
 import config
 from config import LOGGER_ID
-from EQUROBOT.modules import ALL_MODULES
-#from EQUROBOT.modules.clonedb import restart_bots
+from EQUROBOT.modules.clonedb import restart_bots
+
+LOGGER_ID = config.LOGGER_ID
 
 loop = asyncio.get_event_loop()
-
-# Initialize the app here to avoid circular import issues
-app = Client(
-    ":EQUROBOT:",
-    api_id=config.API_ID,
-    api_hash=config.API_HASH,
-    bot_token=config.BOT_TOKEN,
-)
 
 async def daxxpapa_boot():
     for all_module in ALL_MODULES:
         importlib.import_module("EQUROBOT.modules." + all_module)
-    print("Bot successfully started")
-    await app.start()
-#    await restart_bots()
-    await app.send_message(LOGGER_ID, "**I am alive! Your bot has been successfully deployed.\nMy Developer: [𝐌𝚁°᭄𝐃𝙰𝚇𝚇 ࿐™](https://t.me/YourExDestiny)**")
+    print("𝖻𝗈𝗍 𝗌𝗎𝖼𝖼𝖾𝗌𝗌𝖿𝗎𝗅 𝗌𝗍𝖺𝗋𝗍")
+    await restart_bots()
+    await app.send_message(LOGGER_ID, "**𝖨 𝖺𝗆 𝖺𝗅𝗂𝗏𝖾 𝖡𝖺𝖻𝗒 𝖸𝗈𝗎𝗋 𝖡𝗈𝗍 𝖲𝗎𝖼𝖼𝖾𝗌𝗌𝖿𝗎𝗅 𝖣𝖾𝗉𝗅𝗈𝗒 \n Mʏ Dᴇᴠᴇʟᴏᴘᴇʀ  [𝐌𝚁°᭄𝐃𝙰𝚇𝚇 ࿐™ ](https://t.me/YourExDestiny)**")
     await idle()
-    await app.stop()
-    print("Shutting down...")
-
+    print("𝖻𝖺𝗁𝖺𝗇𝖼𝗁𝗈𝖽 𝖯𝗂𝗋𝗈 𝖢𝗈𝖽𝖾𝗋 𝗄𝗋𝗅𝗈 𝖾𝖽𝗂𝗍 𝖺𝖺 𝗀𝗒𝖺 𝗇 𝖾𝗋𝗋𝗈𝗋 𝖺𝖺𝖻 𝗃𝖺𝗄𝖾 𝗀𝖺𝗇𝖽 𝗆𝖺𝗋𝗐𝖺𝗈 𝗂𝗌𝗌𝖾 @YourExDestiny")
+  
+  
 if __name__ == "__main__":
     loop.run_until_complete(daxxpapa_boot())
+    
