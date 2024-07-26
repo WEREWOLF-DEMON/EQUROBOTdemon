19e76271b2e41c457994fd5b7b099a8a1608d0c4
AbhiModszYT <133230865+AbhiModszYT@users.noreply.github.com>
2024-07-26 15:25:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -18,7 +18,7 @@ EVAL = list(map(int, getenv("EVAL", "7427691214 7091230649 6107581019").split())
 # ------------------X------------------------------
 DEEP_API = os.environ.get("DEEP_API", "96a36c8b-0a06-461a-bce3-851d5d997a60")
 # ------------------------------------------------
-LOGGER_ID = int(os.environ.get("LOGGER_ID", "-1002050666864"))
+LOGGER_ID = int(os.environ.get("LOGGER_ID", "-1002196680748"))
 # ------------------------------------------------
 GPT_API = os.environ.get("GPT_API", "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")
 # ------------------------------------------------
