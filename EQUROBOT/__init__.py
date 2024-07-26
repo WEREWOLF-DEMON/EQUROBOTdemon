d739572502ce26833fb535c31b27382a8f0237b7
AbhiModszYT <133230865+AbhiModszYT@users.noreply.github.com>
2024-07-26 14:57:38 +0530

    Update __init__.py

--- EQUROBOT/__init__.py
+++ EQUROBOT/__init__.py
@@ -37,7 +37,7 @@ async def info_bot():
     global BOT_ID, BOT_NAME, BOT_USERNAME
     await app.start()
     await scr.start()
-#    await restart_bots()
+    await restart_bots()
     getme = await app.get_me()
     BOT_ID = getme.id
     BOT_USERNAME = getme.username
