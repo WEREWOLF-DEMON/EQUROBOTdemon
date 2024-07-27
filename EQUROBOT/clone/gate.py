f67a6f3a36451fcd3685189ea91f76ac5bfc74d5
Mohitag403 <149030402+Mohitag403@users.noreply.github.com>
2024-07-27 11:36:13 +0530

    Update gate.py

--- EQUROBOT/clone/gate.py
+++ EQUROBOT/clone/gate.py
@@ -4,7 +4,7 @@ import telebot
 import time
 import mysql.connector
 import json
-from pyrogram import filters
+from pyrogram import Client, filters
 from concurrent.futures import ThreadPoolExecutor
 from itertools import cycle
 from mysql.connector import Error
