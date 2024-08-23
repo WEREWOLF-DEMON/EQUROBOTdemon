7cbaec73ca4f2c86820f91e207ccf851f4a1e175
ᴹᴿᴅᴀxx ࿐™ <147141400+DAXXTEAM@users.noreply.github.com>
2024-08-23 22:24:32 +0530

    Update __init__.py

--- EQUROBOT/__init__.py
+++ EQUROBOT/__init__.py
@@ -1,12 +1,17 @@
 import asyncio
 import logging
-from pyrogram import Client
+import time
+from importlib import import_module
+from os import listdir, path
 from dotenv import load_dotenv
+from pyrogram import Client
+from pyromod import listen
 from config import API_ID, API_HASH, BOT_TOKEN, BOT_USERNAME, OWNER_ID, GPT_API, LOGGER_ID, DEEP_API
 
 from SafoneAPI import SafoneAPI
 
 safone = SafoneAPI()
+
 
 loop = asyncio.get_event_loop()
 
@@ -15,7 +20,8 @@ logging.basicConfig(
     level=logging.INFO,
 )
 
-# Bot client initialization
+
+
 app = Client(
     ":EQUROBOT:",
     api_id=API_ID,
@@ -23,13 +29,14 @@ app = Client(
     bot_token=BOT_TOKEN,
 )
 
-# String session client initialization
 scr = Client(
     "scr",
     api_id=API_ID,
     api_hash=API_HASH,
-    session_string="BQF1_JUAfDIQopgOc0Oan79zOi2QGpJOw0XqCmDMTscNKDlryBZcHw9X1NGBqJvDsaWbSYqaFVnQDwF_HcMK4haNSrIfn2YXM64ZC5Jd5KtiktXX-tSNTk0b4y4t8wBMCWNzw-YZcBJ2BbwPe5YotHH4sAN4S2-3c2bguWoo3pMyHE6RdlJSJ7B6nEHpgrnaVY12wIpnHHdW7_2bXQohHTub_Kr-X_mry1EX3N4QqTVo9Yne-QvhrVuK_R9skv4iPpNV3qv0wpeXXZs6W2iz3azuS9Tltkde0L9MLSq8DwPbP0g_0IjPEZUIZeQEJ2fYbdZxqHehYRfx99tBv-BUTIv_tuTjogAAAABpRCaiAA"
+    session_string="BQGl5vcAK_0OG4Lx7jsDUzmqOP34rErAupITPR3EilRt2XEL6IfNGh6D6Z0WPY0KIeRBNvppdjd8rfJz_N6onphLK0IdJlFr1L_bJGKDFRV7w6szccICn_XwiO4osXWHWwF0iC52zh0hK8rufqEYWvnv9i7sYvFb5qKLZFuHmHYjoWNXJ_YJND-n1-29cgT9iG-TlR6zmOrsRZt45DaNS4CYHq44FFfDzXqWSEScZDLjP7CXZNBo7b6781l1MjUH-gg3nApgiT0AKWd2QlOnwHpd0kkGanbjqW4U_bJVRl77ZoPAF50NX4qyjHD-v7Y7O04WAcIocr-gY9qE12xvGJbJ2r8pQwAAAABpRCaiAA"
 )
+
+
 
 async def info_bot():
     global BOT_ID, BOT_NAME, BOT_USERNAME
@@ -42,5 +49,6 @@ async def info_bot():
         BOT_NAME = getme.first_name + " " + getme.last_name
     else:
         BOT_NAME = getme.first_name
+
 
 loop.run_until_complete(info_bot())
