5172ace08bb2ac997b6617a88f2a4324789c7b60
Mohitag403 <149030402+Mohitag403@users.noreply.github.com>
2024-10-05 20:00:28 +0530

    Update mongo.py

--- EQUROBOT/core/mongo.py
+++ EQUROBOT/core/mongo.py
@@ -1,6 +1,6 @@
 from config import MONGO_DB
 from motor.motor_asyncio import AsyncIOMotorClient as MongoCli
-from datetime import datetime
+from datetime import datetime, timedelta
 from EQUROBOT import app
 
 mongo = MongoCli(MONGO_DB)
@@ -19,3 +19,12 @@ async def has_premium_access(user_id):
         else:
             await premiumdb.update_one({"id": int(user_id)}, {"$set": {"expiry_time": None}})
     return False
+
+
+async def check_remaining_uasge(user_id):
+    user_data = await premiumdb.find_one({"id": int(user_id)})
+    if user_data and user_data.get("expiry_time"):
+        expiry_time = user_data["expiry_time"]
+        remaining_time = expiry_time - datetime.now()
+        return remaining_time
+    return timedelta(0)
