4a90b68d1a5f7dc7f928ff0e12f135ffc37a5890
Mohitag403 <149030402+Mohitag403@users.noreply.github.com>
2024-10-08 21:26:00 +0530

    Update __main__.py

--- EQUROBOT/__main__.py
+++ EQUROBOT/__main__.py
@@ -12,7 +12,7 @@ loop = asyncio.get_event_loop()
 
 async def daxxpapa_boot():
     for all_module in ALL_MODULES:
-        importlib.import_module("EQUROBOT.modules." + all_module)
+        importlib.import_module("EQUROBOT.modules" + all_module)
     print("𝖻𝗈𝗍 𝗌𝗎𝖼𝖼𝖾𝗌𝗌𝖿𝗎𝗅 𝗌𝗍𝖺𝗋𝗍")
     await app.send_message(LOGGER_ID, "**𝖨 𝖺𝗆 𝖺𝗅𝗂𝗏𝖾 𝖡𝖺𝖻𝗒 𝖸𝗈𝗎𝗋 𝖡𝗈𝗍 𝖲𝗎𝖼𝖼𝖾𝗌𝗌𝖿𝗎𝗅 𝖣𝖾𝗉𝗅𝗈𝗒 \n Mʏ Dᴇᴠᴇʟᴏᴘᴇʀ  [𝐌𝚁°᭄𝐃𝙰𝚇𝚇 ࿐™ ](https://t.me/YourExDestiny)**")
     await idle()
